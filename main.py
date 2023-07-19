fb1973b2fc14572f2f0bd38e68bb9bc93f640730
donfortune <donfortunet.df@gmail.com>
2023-07-19 10:06:04 +0100

    added logic

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-API = "xxxxxxxx"
-BASE_URL = "https://free.currconv.com/"   #address to send api request to
-from request import get 
+API = "XXXXXXXXX"
+BASE_URL = "http://api.exchangeratesapi.io/v1/"  # Address to send API request to
+
+from requests import get
+
+endpoint = BASE_URL + "latest?access_key=" + API
+
+# Now you can use the 'endpoint' variable to make the API request
+response = get(endpoint)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    data = response.json()
+    rates = data.get('rates', {})
+    dates = data.get('date', '')
+   
+    print("Date:", dates)
+
+    # Print out all the countries
+    for currency in rates:
+        print(currency)
+    selected_currency = input('Please select your currency:')
+    amount = float(input("How much do you want to convert"))
+    against_currency = input("Select your other currency:")
+    
+    if selected_currency in rates:
+        Selected_rate =  rates[selected_currency]
+    if against_currency in rates:
+        against_rate = rates[against_currency]
+    calculated_rate = amount  * (Selected_rate  / against_rate)
+    print(f"{amount:.2f} {selected_currency} is equivalent to {calculated_rate} {against_currency}")
 
 
+else:
+    # Handle the error appropriately
+    print("Error occurred:", response.text)
